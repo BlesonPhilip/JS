aa511d880cb198fdc0e20b915aba6382c3343560
BlesonPhilip <blesonphilip21@gmail.com>
2025-01-27 17:29:43 +0530

    string methods

--- 05-string.js
+++ 05-string.js
@@ -19,6 +19,27 @@
 // var str = 'Bleson Philip';
 // console.log(str.length);
 
-var str = 'bleson';
-var strlen = str.length;
-console.log(strlen);
+// var str = 'bleson';
+// var strlen = str.length;
+// console.log(strlen);
+
+// var str = 'hello friends';
+
+// console.log(str[4]);
+// console.log(str[str.length - 1]);
+// console.log(str[str.length]);
+
+// String Methods
+
+var str = 'blleson philip';
+// var res = str.toUpperCase();
+// var res= str.toLowerCase();
+// var  res= str .chartAt(4);
+// var res = str.substring(4, 6);
+// var res = str.indexOf('l');
+// var res = str.indexOf('les');
+// var res = str.replace('l', 'e');
+// var res = str.replaceAll('l', 's');
+var res = str.replaceAll('philip', 'Bino');
+
+console.log(res);
